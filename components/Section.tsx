89ff7e5df6572db1b4e45cbf3ff46255764be4c3
Shubham Singh <shubhamsinghcodex@gmail.com>
2023-10-10 18:34:12 +0530

    Made Caraousal

--- components/Section.tsx
+++ components/Section.tsx
@@ -3,13 +3,20 @@ import React from "react";
 interface SectionProp {
   children: React.ReactNode;
   heading?: string;
+  padding?: boolean;
+  margin?: boolean;
 }
 
-const Section = ({ children, heading }: SectionProp): React.ReactNode => {
+const Section = ({
+  children,
+  heading,
+  padding = true,
+  margin = true,
+}: SectionProp): React.ReactNode => {
   return (
-    <section className="max-w-7xl mx-auto py-4 px-6 relative">
+    <section className={" mx-auto relative " + (padding ? " py-4 px-2" : "")}>
       {heading && <h2 className="ralway title my-2 ">{heading}</h2>}
-      <div className="content my-3">{children}</div>
+      <div className={"content " + (margin ? "my-3" : "")}>{children}</div>
     </section>
   );
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import Section from "../components/Section";
 import ImageCard from "../components/ImageCard";
+import Caraousal from "../components/Caraousal";
 import "./App.css";
+
 function App() {
+  const data = [
+    { src: "first.png" },
+    { src: "second.png" },
+    { src: "third.png" },
+    { src: "fourth.png" },
+  ];
+
   return (
-    <Section heading="Categories">
-      <div className="flex flex-row flex-wrap justify-around">
-        <ImageCard type="cat" image="/Categories/first.png" />
-        <ImageCard type="cat" image="/Categories/second.png" />
-        <ImageCard type="cat" image="/Categories/third.png" />
-        <ImageCard type="cat" image="/Categories/fourth.png" />
-        <ImageCard type="cat" image="/Categories/fifth.png" />
-        <ImageCard type="cat" image="/Categories/sixth.png" />
-      </div>
-    </Section>
+    <>
+      <Caraousal data={data} />
+
+      <Section heading="Categories">
+        <div className="flex flex-row flex-wrap justify-around">
+          <ImageCard type="cat" image="/Categories/first.png" />
+          <ImageCard type="cat" image="/Categories/second.png" />
+          <ImageCard type="cat" image="/Categories/third.png" />
+          <ImageCard type="cat" image="/Categories/fourth.png" />
+          <ImageCard type="cat" image="/Categories/fifth.png" />
+          <ImageCard type="cat" image="/Categories/sixth.png" />
+        </div>
+      </Section>
+    </>
   );
 }
 
