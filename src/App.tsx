b31669d1832ebeec25c343efeb375fa0d1513d98
Shubham Singh <shubhamsinghcodex@gmail.com>
2023-10-14 01:30:02 +0530

    Made Navbar responsive, Changed Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar.tsx";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar.tsx";
 import "../css/Navbar.css";
 import App from "./App.tsx";
 import "./index.css";
@@ -28,8 +26,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Navbar />
     <RouterProvider router={router} />
-    <Footer />
   </React.StrictMode>
 );
